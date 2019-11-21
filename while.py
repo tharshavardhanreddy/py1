a4d5f0659e51bbd8e7db952036541f232edabdae
Tatireddy Harshavardhan Reddy <48549064+tharshavardhanreddy@users.noreply.github.com>
2019-11-21 12:00:04 +0530

    Initial commit

---
+++ while.py
@@ -0,0 +1,19 @@
+i = 1
+while i <= 5:
+    print('harsha', end="  ")
+    j = 1
+    while j <= 4:
+        print("reddy", end="  ")
+        j = j + 1
+
+    i = i + 1
+    print()
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i = i + 1
+
+for i in range(1, 20, 2):
+    print(i)
